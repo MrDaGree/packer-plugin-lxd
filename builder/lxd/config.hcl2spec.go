10bda9f0449e933594d58cb3576f2c2cb67a4c9d
MrDaGree <dawson@mrdagree.com>
2023-06-05 21:13:38 -0500

    feat(profiles): changed profile to profiles to support mutliple profiles

--- builder/lxd/config.hcl2spec.go
+++ builder/lxd/config.hcl2spec.go
@@ -18,13 +18,12 @@ type FlatConfig struct {
 	PackerOnError       *string           `mapstructure:"packer_on_error" cty:"packer_on_error" hcl:"packer_on_error"`
 	PackerUserVars      map[string]string `mapstructure:"packer_user_variables" cty:"packer_user_variables" hcl:"packer_user_variables"`
 	PackerSensitiveVars []string          `mapstructure:"packer_sensitive_variables" cty:"packer_sensitive_variables" hcl:"packer_sensitive_variables"`
-	OutputImage         *string           `mapstructure:"output_image" required:"false" cty:"output_image" hcl:"output_image"`
-	OutputAliases       []string          `mapstructure:"output_aliases" required:"false" cty:"output_aliases" hcl:"output_aliases"`
+	OutputAlias         []string          `mapstructure:"output_alias" required:"false" cty:"output_alias" hcl:"output_alias"`
 	ContainerName       *string           `mapstructure:"container_name" cty:"container_name" hcl:"container_name"`
 	PublishRemoteName   *string           `mapstructure:"publish_remote_name" required:"false" cty:"publish_remote_name" hcl:"publish_remote_name"`
 	CommandWrapper      *string           `mapstructure:"command_wrapper" required:"false" cty:"command_wrapper" hcl:"command_wrapper"`
 	Image               *string           `mapstructure:"image" required:"true" cty:"image" hcl:"image"`
-	Profile             *string           `mapstructure:"profile" cty:"profile" hcl:"profile"`
+	Profiles            []string          `mapstructure:"profiles" cty:"profiles" hcl:"profiles"`
 	InitSleep           *string           `mapstructure:"init_sleep" required:"false" cty:"init_sleep" hcl:"init_sleep"`
 	PublishProperties   map[string]string `mapstructure:"publish_properties" required:"false" cty:"publish_properties" hcl:"publish_properties"`
 	LaunchConfig        map[string]string `mapstructure:"launch_config" required:"false" cty:"launch_config" hcl:"launch_config"`
@@ -52,13 +51,12 @@ func (*FlatConfig) HCL2Spec() map[string]hcldec.Spec {
 		"packer_on_error":            &hcldec.AttrSpec{Name: "packer_on_error", Type: cty.String, Required: false},
 		"packer_user_variables":      &hcldec.AttrSpec{Name: "packer_user_variables", Type: cty.Map(cty.String), Required: false},
 		"packer_sensitive_variables": &hcldec.AttrSpec{Name: "packer_sensitive_variables", Type: cty.List(cty.String), Required: false},
-		"output_image":               &hcldec.AttrSpec{Name: "output_image", Type: cty.String, Required: false},
-		"output_aliases":             &hcldec.AttrSpec{Name: "output_aliases", Type: cty.List(cty.String), Required: false},
+		"output_alias":               &hcldec.AttrSpec{Name: "output_alias", Type: cty.List(cty.String), Required: false},
 		"container_name":             &hcldec.AttrSpec{Name: "container_name", Type: cty.String, Required: false},
 		"publish_remote_name":        &hcldec.AttrSpec{Name: "publish_remote_name", Type: cty.String, Required: false},
 		"command_wrapper":            &hcldec.AttrSpec{Name: "command_wrapper", Type: cty.String, Required: false},
 		"image":                      &hcldec.AttrSpec{Name: "image", Type: cty.String, Required: false},
-		"profile":                    &hcldec.AttrSpec{Name: "profile", Type: cty.String, Required: false},
+		"profiles":                   &hcldec.AttrSpec{Name: "profiles", Type: cty.List(cty.String), Required: false},
 		"init_sleep":                 &hcldec.AttrSpec{Name: "init_sleep", Type: cty.String, Required: false},
 		"publish_properties":         &hcldec.AttrSpec{Name: "publish_properties", Type: cty.Map(cty.String), Required: false},
 		"launch_config":              &hcldec.AttrSpec{Name: "launch_config", Type: cty.Map(cty.String), Required: false},
